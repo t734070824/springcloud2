f95218e1441e33f882b3e38557f58cf77b45a5d8
tangqing <734070824@qq.com>
2019-09-17 09:16:02 +0800

    eureka

---
+++ service_discovery/src/main/java/_1_eureka/_client/ProviderUserApplication.java
@@ -0,0 +1,16 @@
+package _1_eureka._client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 734070824@qq.com
+ * @date 2019/9/17 8:53
+ */
+@SpringBootApplication
+public class ProviderUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderUserApplication.class, args);
+    }
+}
---
+++ service_discovery/src/main/java/_1_eureka/_server/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package _1_eureka._server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 734070824@qq.com
+ * @date 2019/9/17 8:40
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
